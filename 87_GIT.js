6727e052ab59ae6c592294bc092f94f4909446b3
bikash032 <bishokarma1996@gmail.com>
2024-12-27 23:45:41 +0545

    Real life example of all revision

--- 87_GIT.js
+++ 87_GIT.js
@@ -37,5 +37,42 @@ d. git commit -m "comment for what you have done for that block of the code" ==>
                                                                                working in the company 
 
 e. git push ==> before the push of the code we need to pull the code it is because we are working on the team that time many people they are 
-               
+     so for this we need to pull the code 
+f. git pull origin remote_branch_name  
+
+
+
+
+
+
+
+    HOW TO MAKE THE NEW BRANCH INSIDE REPOSATORY OF THE MAIN BRANCH?
+===> for making of the verson control we need to make the new branch and need to do push or pull operation for the workflow of the code without 
+the code loss or for maintain of the useability of the code 
+ 
+
+to make new code ===> git checekout -b new_branch_name ==> this will make new branch with taking all of the code and the properties form the last
+                     branch . take all the code and properties of the previous branch
+to just switch the pri existing brannch
+===>> git checkout branch_to_switch_branch_name  ==> this will just switch the branch but not bring all the code and features of the branch
+
+
+*******************VVVVVVIIIIIIPPPPP****************
+If there is the operation within of the same local remote and branch code of the local directory then it will merge by pulling of the same code
+and it will not give any short of the conflict 
+syntex======>>> git merge bikash  ===> for this the main branch(current branch=main) will update or merze all the channges that are done within
+of the bikash as the remote branch 
+
+now after i merge to my main branches and if there is no one pushes code for the main branch then i can simply 
+*********git push origin main********** 
+
+
+but 
+
+if we want to pull the git reposatory code from git to the local remote branches then if there is same code or logic for the same of the code block
+then it will give the conflict for the merzing of the code. 
+conflict for the merzaing of the coded
+
+
+
 */
