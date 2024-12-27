ab04cb4ced7eab5a125d2b91df77df30188eb2cc
bikash032 <bishokarma1996@gmail.com>
2024-12-27 18:25:29 +0545

    new introduction of git

---
+++ 87_GIT.js
@@ -0,0 +1,41 @@
+/*
+****************GIT INTRODUCTION************
+
+When we working on the company to make the flow of the working balanced and to share the block of the code within the team, we can use 
+this type of the application. As this is the imprtant for each and most of the company for the deoloyation and to edit each other block of 
+code. 
+
+for this we need to install first the git latest verson to our pc
+to varyfy we can run command inside the terminal of the laptop or as powercell of the window  
+      git -v
+      this will give the latest verson of the git inside of our pc
+
+
+
+      as when we clone any things from any of the reposotary first it need to be in public or if there is private then we need to be accesiable 
+      by adding of them in the group list by using gmail id or the github id
+
+
+there we can do many operation inside of the git they are
+1. PUSH OF THE CODE FOR THE NEW REPOSOTARY ====>>
+                                           IF some one give you the new reposatory and if the reposatory is empty for that case need to push our 
+                                           code then at first we need to init our directory and to be make sure that the used directoty need 
+                                           to contain of the git and afer that we need to maintain that reposotary in the file.
+this type of the reposatory creation we can do once in one directry as after we are using of the below command that we can use for after of the 
+first initialization. They are:
+
+
+
+a. git init
+b. git remote add origin "url of reposotary"  
+       a and b are written once when we start of the particular or the each of the project. new project = new reposatory 
+c. git add .  ====> this will push all of the code that are inside of the directory where we initialiaze the git reposotary..git   
+              or git add "file name" ==> this will push only the desired of the block of the code fo the particular of the code of the block
+
+d. git commit -m "comment for what you have done for that block of the code" ==> this is most of the important of the block of the code that is 
+                                                                               being to be tracked by the other people what you have done while 
+                                                                               working in the company 
+
+e. git push ==> before the push of the code we need to pull the code it is because we are working on the team that time many people they are 
+               
+*/
